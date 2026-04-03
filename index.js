4ca2b776116901439c4572686f91848925f2443b
André232007 <brawlls271@gmail.com>
2026-04-02 22:08:16 -0300

    Corrigindo falta do Chrome no Render

--- index.js
+++ index.js
@@ -164,7 +164,16 @@ async function processCommand(message, command, sender) {
 const client = new Client({
     authStrategy: new LocalAuth(),
     puppeteer: {
-        args: ['--no-sandbox', '--disable-setuid-sandbox']
+        executablePath: '/usr/bin/google-chrome-stable',
+        args: [
+            '--no-sandbox',
+            '--disable-setuid-sandbox',
+            '--disable-dev-shm-usage',
+            '--disable-accelerated-2d-canvas',
+            '--no-first-run',
+            '--no-zygote',
+            '--disable-gpu'
+        ]
     }
 });
 
